7d0066d1b599089010ba1cd5cb04a8da3a567ffd
raj patel <rajpatel9515@gmail.com>
2020-08-15 13:53:20 +0530

    .

---
+++ scr/HELLO.ino
@@ -0,0 +1,18 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(1,2);
+  lcd.print("HELLO");
+}
+
+
+void loop()
+{
+}
